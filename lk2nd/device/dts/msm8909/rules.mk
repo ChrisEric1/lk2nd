c0ea16e8ec6e6f1baf26d3b16bb5e82a4f26f131
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2023-09-08 15:43:02 -0400

    New DTBs (stock kernel)

--- lk2nd/device/dts/msm8909/rules.mk
+++ lk2nd/device/dts/msm8909/rules.mk
@@ -2,4 +2,6 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 ADTBS += \
-	$(LOCAL_DIR)/msm8909-catfish.dtb \
+        $(LOCAL_DIR)/catfish_1g_ddr.dtb \
+        $(LOCAL_DIR)/catshark.dtb \
+        $(LOCAL_DIR)/catfish.dtb
